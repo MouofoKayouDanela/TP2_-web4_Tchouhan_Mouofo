75273d7ba135a8df333eaa93fc9233c6c0efd6b7
MouofoKayouDanela <mouofodanela@gmail.com>
2026-01-12 22:10:32 -0500

    config swagger

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -312,7 +312,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'https://tp2-web4-tchouhan-mouofo-main-b6sher.laravel.cloud/api'),
         ],
     ],
 ];
